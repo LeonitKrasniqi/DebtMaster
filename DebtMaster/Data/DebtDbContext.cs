c7e0fb1f3dcc17dec36b536593805e688fa0e327
LeonitKrasniqi <leonitkr88@gmail.com>
2024-04-29 15:53:22 +0200

    Connect server with database and adding some Entity Frameworks Libraries

---
+++ DebtMaster/Data/DebtDbContext.cs
@@ -0,0 +1,20 @@
+﻿using DebtMaster.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace DebtMaster.Data
+{
+    public class DebtDbContext : DbContext
+    {
+        public DebtDbContext(DbContextOptions<DebtDbContext> debtDbContext) : base(debtDbContext)
+        {
+            
+        }
+
+        public DbSet<Debt> Debts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+             
+        }
+    }
+}
---
+++ DebtMaster/Models/Domain/Debt.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DebtMaster.Models.Domain
+{
+    public class Debt
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string DebtorName { get; set; }
+
+    }
+}
