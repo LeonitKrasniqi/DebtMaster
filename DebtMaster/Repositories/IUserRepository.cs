557e64ffc366982789c64299f3e3861dcf0852af
LeonitKrasniqi <leonitkr88@gmail.com>
2024-06-02 18:53:10 +0200

    Created a DTO for user, declared and implemented CRUD operations for User Entity.

---
+++ DebtMaster/Repositories/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using DebtMaster.Models.Domain;
+
+namespace DebtMaster.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User> CreateAsync(User user);
+        Task<List<User>> GetAllAsync();
+        Task<User> GetByIdAsync(Guid id);
+        Task<User> UpdateAsync(User user);
+        Task<bool> DeleteAsync(Guid id);
+
+    }
+}
---
+++ DebtMaster/Repositories/UserRepository.cs
@@ -0,0 +1,53 @@
+﻿using DebtMaster.Data;
+using DebtMaster.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace DebtMaster.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DebtDbContext dbContext;
+
+        public UserRepository(DebtDbContext dbContext)
+        {
+             this.dbContext = dbContext;
+        }
+        public async Task<User> CreateAsync(User user)
+        {
+           dbContext.Users.Add(user);
+            await dbContext.SaveChangesAsync();
+            return user;
+        }
+
+        public async Task<bool> DeleteAsync(Guid id)
+        {
+            var user = await dbContext.Users.FindAsync(id);
+            if (user == null)
+            {
+                return false;
+            }
+
+            dbContext.Users.Remove(user);
+            await dbContext.SaveChangesAsync();
+            return true;
+
+        }
+
+        public async Task<List<User>> GetAllAsync()
+        {
+             return await dbContext.Users.ToListAsync();
+        }
+
+        public async Task<User> GetByIdAsync(Guid id)
+        {
+            return await dbContext.Users.FirstOrDefaultAsync(x => x.UserID == id);
+        }
+
+        public async Task<User> UpdateAsync(User user)
+        {
+            dbContext.Users.Update(user);
+            await dbContext.SaveChangesAsync();
+            return user;
+        }
+    }
+}
