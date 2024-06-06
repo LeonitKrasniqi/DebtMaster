c037cb5d125789eb16447158d43b0d0aa3d68e7c
LeonitKrasniqi <leonitkr88@gmail.com>
2024-06-06 22:22:02 +0200

    Implemented method to register and log in user in Controller. Return a JWT. Adding in Program.cs validation for password, validation for jwt and added authorization in swagger ui testing.

---
+++ DebtMaster/Models/DTOs/RegsiterRequestDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DebtMaster.Models.DTOs
+{
+    public class RegsiterRequestDto
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public string[] Roles { get; set; }
+    }
+}
